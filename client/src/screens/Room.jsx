0da1d43f82050f42425c7909da566f349239a38c
Manobhiramreddy <149897547+Manobhiramreddy@users.noreply.github.com>
2023-11-30 18:26:30 +0530

    Update Room.jsx

--- client/src/screens/Room.jsx
+++ client/src/screens/Room.jsx
@@ -111,7 +111,7 @@ const RoomPage = () => {
 
   return (
     <div>
-      <h1>Room Page</h1>
+      <h1>ROOM ENTERED!</h1>
       <h4>{remoteSocketId ? "Connected" : "No one in room"}</h4>
       {myStream && <button onClick={sendStreams}>Send Stream</button>}
       {remoteSocketId && <button onClick={handleCallUser}>CALL</button>}
